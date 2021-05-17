d4b1d7a95f578ef8f4dac4c362169974d57d3e21
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-17 11:28:09 -0300

    remove task button created

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -3,12 +3,13 @@ import "./index.css";
 
 const Task = ({ task, handleTaskClick }) => {
     return (
-        <div
-            className="task-container"
-            style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
-        >
+        <div className="task-container" style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}>
             <div className="task-title" onClick={() => handleTaskClick(task.id)}>
                 {task.title}
+            </div>
+
+            <div className="buttons-container">
+                <button className="remove-task-button">X</button>
             </div>
         </div>
     );

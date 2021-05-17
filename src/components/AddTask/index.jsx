01ee4d031245391469ad18086fa016d370680995
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-17 10:57:38 -0300

    inputData handling

--- src/components/AddTask/index.jsx
+++ src/components/AddTask/index.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import "./index.css"
 
 const AddTask = () => {
+    
+    const [inputData, setInputData] = useState("");
+
+    const handleInputChange = (text) => {
+        setInputData(text.target.value);
+    }
+
     return (
         <div className="add-task-container">
-            <input className="add-task-input" type="text" />
+            <input className="add-task-input" onChange={handleInputChange} value={inputData} type="text" />
             <div className="add-task-button-container">
                 <Button>
                     Add

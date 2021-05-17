26cdf530a8c3b3ac95f5fdc3aa84fe38a3898644
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-17 10:00:53 -0300

    Tasks component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Tasks from './components/Tasks';
 
 const App = () => {
-  const message = 'Hello, World!';
-  return(
+  // let message = 'Hello, World!';
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Study programming',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Read books',
+      completed: true
+    }
+  ]);
+
+  return (
     <div className="container">
-      
+      <Tasks/>
     </div>
   )
 }
